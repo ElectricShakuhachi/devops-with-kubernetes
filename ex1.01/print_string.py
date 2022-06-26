8a7d6cc6ffc4ee53a6819e4fcecfbca7671555db
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-06-27 00:31:44 +0300

    Do exercises until 1.05

--- ex1.01/print_string.py
+++ ex1.01/print_string.py
@@ -3,7 +3,6 @@ from time import sleep, ctime
 with open("string.txt", "r") as file:
     random_string = file.read()
 
-for i in range(10):
+while True:
     print(ctime() + ": " + random_string)
     sleep(5)
-    
